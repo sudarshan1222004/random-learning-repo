e79837b11e2f53b3bbff95ffe43c4e2f9ec37b84
sudarshan1222004 <sudarshanmadival2004@gmail.com>
2025-09-07 15:26:03 +0530

    py file

---
+++ app.py
@@ -0,0 +1,12 @@
+num1 = 10
+num2 = 5
+
+sum_result = num1 + num2
+difference_result = num1 - num2
+product_result = num1 * num2
+division_result = num1 / num2
+
+print(f"Sum: {sum_result}")
+print(f"Difference: {difference_result}")
+print(f"Product: {product_result}")
+print(f"Division: {division_result}")
